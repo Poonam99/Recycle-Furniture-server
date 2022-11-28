bee3b700789c17fd89a1e9dbd90365c05a22eead
Poonam99 <poonamtabassum9909@gmail.com>
2022-11-28 12:16:55 +0600

    Server Created

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Server Running')
+})
+
+app.listen(port, () => {
+    console.log(`Server Loading on port: ${port}`);
+})
