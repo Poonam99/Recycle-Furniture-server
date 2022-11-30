66c66faf63ed01360d321583c13cb737147e1d67
Poonam99 <poonamtabassum9909@gmail.com>
2022-11-30 18:31:34 +0600

    database connected

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
-const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const axios = require('axios').default;
+const port = process.env.PORT || 5000;
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 
 
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yi8iiuw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 
 app.get('/', (req, res) => {
